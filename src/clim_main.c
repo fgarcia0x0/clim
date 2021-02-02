89e9c37028bc702500e3ed160dc9692f80d40f49
Felipe Garcia <felipegarcia1402@gmail.com>
2021-02-02 15:38:42 -0300

    Update clim_main.c

--- src/clim_main.c
+++ src/clim_main.c
@@ -1,16 +1,18 @@
-#include "..\include\clim_base.h"
-#include "..\include\clim_img.h"
-#include "..\include\clim_logger.h"
-#include "..\include\clim_utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/clim_base.h"
+#include "../include/clim_img.h"
+#include "../include/clim_logger.h"
+#include "../include/clim_utils.h"
 
 int main()
 {
-	clim_log_init("clim_log", ".txt");
-
-	CLIM_LOG_INFO("Init Commit");
-	CLIM_LOG_WARN("Warn Ocurred");
-	CLIM_LOG_ERROR("Fail Ocurred");
-
-	clim_log_close();
-	return 0;
+	#ifdef CLIM_DEBUG_MODE
+		CLIM_LOG_DEBUG("Running in Debug Mode");
+	#else
+		CLIM_LOG_INFO("Running in Release Mode");
+	#endif
+		
+	CLIM_LOG_INFO("clim has been initialized");
 }
