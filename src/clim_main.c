c0691e36cec8f2cb9143029744f857dc0cdf01a2
Felipe Garcia <felipegarcia1402@gmail.com>
2021-03-09 21:06:50 -0300

    update main

--- src/clim_main.c
+++ src/clim_main.c
@@ -1,18 +1,29 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 
 #include "../include/clim_base.h"
 #include "../include/clim_img.h"
 #include "../include/clim_logger.h"
 #include "../include/clim_utils.h"
+#include "../include/clim_filesystem.h"
 
-int main()
+static inline void clim_init()
 {
-	#ifdef CLIM_DEBUG_MODE
-		CLIM_LOG_DEBUG("Running in Debug Mode");
-	#else
-		CLIM_LOG_INFO("Running in Release Mode");
+	#ifdef CLIM_OS_WIN
+		CLIM_DO_IF(
+			!SetConsoleOutputCP(CLIM_WIN_UTF8_CODEPAGE),
+			CLIM_LOG_WARN("Fail to configure console codepage to utf8")
+		);
 	#endif
 
-	CLIM_LOG_INFO("clim has been initialized");
+	#ifdef CLIM_DEBUG_MODE
+		CLIM_LOG_DEBUG("Running in Debug Mode");
+	#endif
+
+	CLIM_LOG_DEBUG("clim has been initialized");
+}
+
+int main(int argc, char* argv[])
+{
+	clim_init();
 }
